ca3c4e46779bb9484ba618a02dc91aa2c7662bc5
Ubong Effiong <pendusky@gmail.com>
2022-09-08 21:44:05 -0700

    4 putc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,25 +3,21 @@
 
 
 /**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
-* main - print the string in the put function
-
-*
-
-* Decription: using the main function
-
-* this program prints "Programming is like building a multilingual puzzle
-
-* Return: 0
-
-*/
-
-init main(void)
+int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+			return (0);
 
 }
