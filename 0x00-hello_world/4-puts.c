e299b8515745937755a662399e07771a5fd0cad6
Ubong Effiong <pendusky@gmail.com>
2022-09-11 11:34:18 -0700

    4PutsC

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - entry point
  *
- * Return: 0 after printing the function
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
