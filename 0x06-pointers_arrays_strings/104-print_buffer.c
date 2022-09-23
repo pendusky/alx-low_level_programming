6d48f856ce84a897f845ab1d063c774c0833d77e
Ubong Effiong <pendusky@gmail.com>
2022-09-23 14:53:15 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,31 +1,103 @@
 #include "main.h"
+#include <stdio.h>
+
 
 /**
-*  _strncpy - C function that copies a string, including the
-*  terminating null byte, using at most an inputted number of bytes.
-*  If the length of the source string is less than the maximum byte number,
-*  the remainder of the destination string is filled with null bytes.
-*  Works identically to the standard library function `strncpy`.
-*@dest: buffer storing the string copy
-*@src:the source string
-*@n:max nummber of byte copied
-*Return: returns
-*/
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: void
+ */
 
-char *_strncpy(char *dest, char *src, int n)
+void print_line(char *c, int s, int l)
 
+{
+
+	int j, k;
+
+
+
+	for (j = 0; j <= 9; j++)
+
+	{
+
+		if (j <= s)
+
+			printf("%02x", c[l * 10 + j]);
+
+		else
+
+			printf("  ");
+
+		if (j % 2)
+
+			putchar(' ');
+
+	}
+
+	for (k = 0; k <= s; k++)
+
+	{
+
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+
+			putchar(c[l * 10 + k]);
+
+		else
+
+			putchar('.');
+
+	}
+
+}
+
+
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
 {
 
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
 
-		dest[i] = src[i];
 
-	for ( ; i < n; i++)
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
 
-		dest[i] = '\0';
+	{
 
-	return (dest);
+		printf("%08x: ", i * 10);
+
+		if (i < size / 10)
+
+		{
+
+			print_line(b, 9, i);
+
+		}
+
+		else
+
+		{
+
+			print_line(b, size % 10 - 1, i);
+
+		}
+
+		putchar('\n');
+
+	}
+
+	if (size == 0)
+
+		putchar('\n');
 
 }
