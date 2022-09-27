fe5e187e7ce62b0afe5c14f279a7c3e4697a2582
Ubong Effiong <pendusky@gmail.com>
2022-09-27 15:12:15 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,30 +1,24 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * _memset - Fill memory with a constant byte
-  * @s: memory area to fill
-  * @b: constant byte to fill
-  * @n: bytes of memory area to fill
-  *
-  * Return: the memory area filled
+  * _memset - fills memory with a constant byte
+  * @s: memory area
+  * @b: the byte
+  * @n: # of bytes to fill
+  * Return: pointer to s
   */
 
 char *_memset(char *s, char b, unsigned int n)
+
 {
 
-	unsigned int a;
+	char *ptr = s;
 
 
 
-	for (a = 0; a < n; a++)
+	while (n-- > 0)
 
-	{
-
-		s[a] = b;
-
-	}
-
-
+	*ptr++ = b;
 
 	return (s);
 
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,10 @@
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+  *Auth:Ubong Effiong
+  */
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -6,5 +12,10 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+int _putchar(char c);
+
+#endif /*MAIN_H*/
+
+
